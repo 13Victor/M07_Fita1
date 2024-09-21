d33663106346fc3e4cc704551b264187dac67db1
Victor <vvalerolopez.eb@iesesteveterradas.cat>
2024-09-22 00:33:24 +0200

    Barcos colocados falta colisiones entre ellos

--- Batalla_naval.php
+++ Batalla_naval.php
@@ -25,22 +25,36 @@
 		<?php
 			$letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
 			$vaixells = [
-				"submari" => [array(), 2, "red"],
-				"aaaaaaa" => [array(), 3, "blue"]
+				"fragata" => [array(), 1, "red"],
+				"submari" => [array(), 2, "blue"],
+				"destructor" => [array(), 3, "green"],  
+				"portaavions" => [array(), 4, "black"]  
 			];
 
-			foreach ($vaixells as &$vaixell) {
+			foreach ($vaixells as $nombre => $vaixell) {
 				$ship_length = $vaixell[1];
 				$ship_positions = [];
-				$start_row = rand(1, 10 - $ship_length + 1);
-				$start_col = $letras[rand(0, count($letras) - 1)];
+
+				// Elegir aleatoriamente la orientación del barco
+				$orientation = rand(0, 1); // 0 = horizontal, 1 = vertical
 				
-				// Horizontal
-				for ($j = 0; $j < $ship_length; $j++) {
-					$ship_positions[] = [$start_row + $j, $start_col];
+				if ($orientation == 0) { // Horizontal
+					$start_row = rand(1, 10);
+					$start_col = rand(0, 10 - $ship_length); // Ajustamos la columna para que el barco no se salga del tablero
+
+					for ($j = 0; $j < $ship_length; $j++) {
+						$ship_positions[] = [$start_row, $letras[$start_col + $j]];
+					}
+				} else { // Vertical
+					$start_row = rand(1, 10 - $ship_length); // Ajustamos la fila para que el barco no se salga del tablero
+					$start_col = rand(0, 9);
+
+					for ($j = 0; $j < $ship_length; $j++) {
+						$ship_positions[] = [$start_row + $j, $letras[$start_col]];
+					}
 				}
 
-				$vaixell[0] = $ship_positions; // Asigna las posiciones
+				$vaixells[$nombre][0] = $ship_positions; // Asignar las posiciones al array original
 			}
 
 			$n = 10;
@@ -58,19 +72,21 @@
 
 				// Recorremos las columnas
 				for ($i = 1; $i <= $n; $i++) {
-					$ship_color = null; 
+					$ship_found = false;
+					$ship_color = null;
 
 					// Verificar si en esta celda hay un barco
 					foreach ($vaixells as $vaixell) {
 						foreach ($vaixell[0] as $pos) {
 							if ($i == $pos[0] && $pos[1] == $letra) {
+								$ship_found = true;
 								$ship_color = $vaixell[2];
 							}
 						}
 					}
 
 					// Pintamos la celda
-					if ($ship_color) {
+					if ($ship_found) {
 						echo "<td style='background: $ship_color;'></td>";
 					} else {
 						echo "<td></td>";
